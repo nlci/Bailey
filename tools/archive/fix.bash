a5eba70fa2e9ae82edf2034afdcaac499f09106d
Bobby de Vos <bobby_devos@sil.org>
2021-12-07 13:39:25 -0700

    Make glyph inventories identical

--- tools/archive/fix.bash
+++ tools/archive/fix.bash
@@ -4,9 +4,9 @@ do
     psfsetversion $ufo 0.200
     $ar/reverse-direction.py $ufo
 done
-psfrenameglyphs -i $ar/rename-bold.csv Bailey-B*.ufo
-psfgetglyphnames -i $ar/copy.txt Bailey-R*.ufo glyphs.csv
+psfgetglyphnames -i $ar/copy-regular.txt Bailey-R*.ufo glyphs.csv
 psfcopyglyphs --unicode usv -s Bailey-R*.ufo -i glyphs.csv Bailey-B*.ufo # --rename rename
+psfcopyglyphs -s Bailey-B*.ufo -i $ar/copy-bold.csv Bailey-R*.ufo
 
 psfbuildcompgc -i $ar/decomposites-bold.glyphConstruction Bailey-B*.ufo
 psfbuildcompgc -i $ar/decomposites.glyphConstruction Bailey-B*.ufo
@@ -19,5 +19,9 @@ psfbuildcompgc -i $ar/composites-bold.glyphConstruction Bailey-B*.ufo
 psfbuildcompgc -i $ar/composites.glyphConstruction Bailey-B*.ufo
 psfbuildcompgc -i $ar/composites.glyphConstruction Bailey-R*.ufo
 
+psfdeleteglyphs -i $ar/delete-bold.txt Bailey-B*.ufo
 psfdeleteglyphs -i $ar/delete.txt Bailey-B*.ufo
 psfdeleteglyphs -i $ar/delete.txt Bailey-R*.ufo
+
+psfrenameglyphs -i $ar/rename-bold.csv Bailey-B*.ufo
+psfsetunicodes -i $ar/encode-bold.csv Bailey-B*.ufo
